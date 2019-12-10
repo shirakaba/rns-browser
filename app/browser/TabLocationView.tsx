b75deddc24c543a9e0bdf31d790472f4b8ff440a
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-12-10 03:44:17 +0000

    Enable props-passing, and try TabLocationView as GridLayout

--- app/browser/TabLocationView.tsx
+++ app/browser/TabLocationView.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { WebView, ActionBar, StackLayout } from "@nativescript/core";
-import { $WebView, $ActionBar, $StackLayout, $FlexboxLayout, $ContentView, $Image, $TextField } from "react-nativescript";
+import { $WebView, $ActionBar, $StackLayout, $FlexboxLayout, $ContentView, $Image, $TextField, $GridLayout } from "react-nativescript";
 import { ToolbarButton } from "./ToolbarButton";
 import { PrivacyIndicatorView } from "~/Views/PrivacyIndicatorView";
 import { TextFieldComponentProps } from "react-nativescript/dist/components/TextField";
+import { ItemSpec } from "tns-core-modules/ui/layouts/grid-layout/grid-layout";
+import { ButtonComponentProps } from "react-nativescript/dist/components/Button";
 
 interface Props {
 
@@ -22,13 +24,13 @@ const TabLocationViewUX = {
 }
 
 // https://github.com/cliqz/user-agent-ios/blob/develop/Client/Frontend/Browser/TabLocationView.swift#L83
-class LockImageView extends React.Component<{ locked: boolean }, {}> {
+class LockImageView extends React.Component<{ locked: boolean } & ButtonComponentProps, {}> {
     render(){
-        const { locked } = this.props;
+        const { locked, ...rest } = this.props;
 
         return (
             // <$Image/>
-            <ToolbarButton text={ locked ? "\uf023" : "\uf3c1" }/>
+            <ToolbarButton text={ locked ? "\uf023" : "\uf3c1" } {...rest}/>
         );
     }
 }
@@ -56,19 +58,23 @@ class UrlTextField extends React.Component<TextFieldComponentProps, {}> {
 }
 
 // https://github.com/cliqz/user-agent-ios/blob/develop/Client/Frontend/Browser/TabLocationView.swift#L111
-class PageOptionsButton extends React.Component<{}, {}> {
+class PageOptionsButton extends React.Component<{} & ButtonComponentProps, {}> {
     render(){
+        const { ...rest } = this.props;
+
         return (
-            <ToolbarButton text={"\uf141"}/>
+            <ToolbarButton text={"\uf141"} {...rest}/>
         );
     }
 }
 
 // https://github.com/cliqz/user-agent-ios/blob/develop/Client/Frontend/Browser/TabLocationView.swift#L105
-class PrivacyIndicator extends React.Component<{}, {}> {
+class PrivacyIndicator extends React.Component<{} & ButtonComponentProps, {}> {
     render(){
+        const { ...rest } = this.props;
+
         return (
-            <PrivacyIndicatorView/>
+            <PrivacyIndicatorView {...rest}/>
         );
     }
 }
@@ -87,26 +93,31 @@ export class TabLocationView extends React.Component<Props, State>{
 
                 {/* https://github.com/cliqz/user-agent-ios/blob/develop/Client/Frontend/Browser/TabLocationView.swift#L149 */}
                 {/* https://developer.apple.com/documentation/uikit/uistackview */}
-                <$FlexboxLayout
-                    flexDirection={"row"}
-                    // May need to change to "stretch"
-                    alignItems={"center"}
-                    justifyContent={"space-around"}
-                    flexWrap={"nowrap"}
+                <$GridLayout
+                    // width={{ value: 100, unit: "%" }}
+                    rows={[new ItemSpec(1, "star")]}
+                    columns={[
+                        new ItemSpec(1, "auto"),
+                        new ItemSpec(1, "auto"),
+                        new ItemSpec(1, "auto"),
+                        new ItemSpec(1, "auto"),
+                        new ItemSpec(1, "star"),
+                        new ItemSpec(1, "auto"),
+                    ]}
                 >
                     {/* frontSpaceView */}
-                    <$ContentView width={{ value: TabLocationViewUX.Spacing, unit: "dip" }}/>
+                    <$ContentView row={0} col={0} width={{ value: TabLocationViewUX.Spacing, unit: "dip" }}/>
 
                     {/* privacyIndicator */}
-                    <PrivacyIndicator/>
+                    <PrivacyIndicator row={0} col={1}/>
                     
                     {/* privacyIndicatorSeparator */}
-                    <$ContentView width={{ value: 3, unit: "dip" }}/>
-                    <LockImageView locked={true}/>
+                    <$ContentView row={0} col={2} width={{ value: 3, unit: "dip" }}/>
+                    <LockImageView row={0} col={3} locked={true}/>
                     {/* FIXME: width */}
-                    <UrlTextField />
-                    <PageOptionsButton/>
-                </$FlexboxLayout>
+                    <UrlTextField row={0} col={4}/>
+                    <PageOptionsButton row={0} col={5}/>
+                </$GridLayout>
             </$StackLayout>
         );
     }
