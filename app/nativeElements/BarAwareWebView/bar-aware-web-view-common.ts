74a60ae339328f0eab70c067ebb833ad49dc3061
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-12-17 21:54:51 +0000

    BarAwareWebViewBase common

---
+++ app/nativeElements/BarAwareWebView/bar-aware-web-view-common.ts
@@ -0,0 +1,26 @@
+// import { WebView as WebViewDefinition } from ".";
+// import { LoadEventData, NavigationType, ProgressEventData } from "./bar-aware-web-view-interfaces";
+import { WebViewBase } from "../../NativeScriptCoreUIForks/WebView/web-view-common";
+import { ContainerView, Property, EventData, CSSType } from "@nativescript/core/ui/core/view";
+import { File, knownFolders, path } from "../../NativeScriptCoreUIForks/WebView/web-view-common";
+
+export { File, knownFolders, path };
+// export * from "./bar-aware-web-view-interfaces";
+export * from "@nativescript/core/ui/core/view";
+
+@CSSType("BarAwareWebView")
+export abstract class BarAwareWebViewBase extends WebViewBase {
+
+}
+
+// HACK: Use an interface with the same name, so that the class above fulfills the 'implements' requirement
+// HACK: We use the 'implements' to verify the class above is the same as the one declared in the d.ts
+// HACK: We declare all these 'on' statements, so that they can appear in the API reference
+// HACK: Do we need this? Is it useful? There are static fields to the WebViewBase class for the event names.
+export interface BarAwareWebViewBase {
+    // on(eventNames: string, callback: (data: EventData) => void, thisArg?: any);
+    // on(event: "progress", callback: (args: ProgressEventData) => void, thisArg?: any);
+    // on(event: "loadFinished", callback: (args: LoadEventData) => void, thisArg?: any);
+    // on(event: "loadCommitted", callback: (args: LoadEventData) => void, thisArg?: any);
+    // on(event: "loadStarted", callback: (args: LoadEventData) => void, thisArg?: any);
+}
